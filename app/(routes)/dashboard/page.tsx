4ebb2c954b05ffd9d1172067b28319e715a67a32
Cathy <leeyu823@gmail.com>
2025-12-02 23:03:13 -0800

    add: dashboard route

---
+++ app/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+        Dashboard Page
+      </h1>
+    </div>
+  )
+}
