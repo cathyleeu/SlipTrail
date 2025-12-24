a0ff74742f5bfb1e569a272914c19ca061b74146
Cathy <leeyu823@gmail.com>
2025-12-23 22:29:37 -0800

    feat: refactor Map component to accept props for location, zoom, and className

--- app/components/map/index.tsx
+++ app/components/map/index.tsx
@@ -1,39 +1,50 @@
-'use client';
+'use client'
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import { useEffect } from 'react';
+import type { GeoLocation } from '@types'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import { useEffect } from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
-export default function Map() {
+type MapProps = {
+  location?: GeoLocation | null
+  zoom?: number
+  className?: string
+}
+
+export default function Map({ location, zoom = 15, className = 'h-64 w-full' }: MapProps) {
   // Fix for default marker icon
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const DefaultIcon = L.Icon.Default as any;
-    delete DefaultIcon.prototype._getIconUrl;
+    const DefaultIcon = L.Icon.Default as any
+    delete DefaultIcon.prototype._getIconUrl
     L.Icon.Default.mergeOptions({
-      iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+      iconRetinaUrl:
+        'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
       iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
       shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-    });
-  }, []);
+    })
+  }, [])
+
+  const center: [number, number] = location ? [location.lat, location.lng] : [49.2827, -123.1207]
 
   return (
     <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
+      center={center}
+      zoom={zoom}
       scrollWheelZoom={false}
-      style={{ height: '100vh', width: '100%' }}
+      className={className}
+      style={{ borderRadius: '12px' }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {location && (
+        <Marker position={[location.lat, location.lng]}>
+          <Popup>{location.address}</Popup>
+        </Marker>
+      )}
     </MapContainer>
-  );
+  )
 }
