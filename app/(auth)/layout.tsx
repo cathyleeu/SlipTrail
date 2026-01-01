ae55457b9d4693aaadb1f8493088bccce0983cdd
Cathy <leeyu823@gmail.com>
2025-12-31 19:37:49 -0800

    add: auth layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { Header } from '@components'
+import { usePathname } from 'next/navigation'
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+
+  const isHideHeader = pathname === '/onboarding'
+  return (
+    <div className="h-screen flex flex-col justify-between">
+      {!isHideHeader && <Header title={''} showBack />}
+      <div className={`overflow-auto ${isHideHeader ? 'h-screen' : 'h-[calc(100vh-57px)]'}`}>
+        {children}
+      </div>
+    </div>
+  )
+}
